81c55970919a54d81e072accd25defc011b177b0
dappnodedev <diego@dappnode.io>
2023-10-13 18:14:18 +0200

    Implement retry system

--- operator/entrypoint.sh
+++ operator/entrypoint.sh
@@ -1,6 +1,66 @@
 #!/bin/bash
 
 NODE_PUBKEY_FILE_PATH=/root/.lighthouse/v1/${OPERATOR_NETWORK}/node_key.json
+DAPPMANAGER_ALIASES=("my.dappnode" "dappmanager.dappnode" "dappmanager.dnp.dappnode.eth.dappmanager.dappnode" "172.33.1.7")
+
+MAX_RETRIES=5
+SLEEP_DURATION=10
+
+function retry_request() {
+    local callback=$1
+    local max_retries=${2:-5}    # Default to 5 retries if not provided
+    local sleep_duration=${3:-5} # Default to 5 seconds if not provided
+
+    local retry_count=0
+    local success=false
+    local result=""
+
+    while [ $retry_count -lt $max_retries ] && [ $success == false ]; do
+        for alias in "${DAPPMANAGER_ALIASES[@]}"; do
+            result=$($callback "$alias")
+
+            # Check if callback succeeded
+            if [[ $? -eq 0 ]]; then
+                # Echoing only the result upon successful callback
+                echo "$result"
+                return 0
+            else
+                echo "[ERROR] Failed request to ${alias} on attempt $((retry_count + 1))"
+            fi
+        done
+
+        # If request was unsuccessful, sleep before retrying
+        if [ $success == false ]; then
+            sleep $sleep_duration
+            ((retry_count++))
+        fi
+    done
+
+    return 1
+}
+
+# Callback function for posting the NODE_PUBKEY
+function post_pubkey() {
+    local alias=$1
+    curl -X POST "http://${alias}/data-send?key=pubkey&data=${NODE_PUBKEY}"
+    return $?
+}
+
+# Callback function for getting the PUBLIC_IP
+function get_public_ip() {
+    local alias=$1
+    local result=$(curl -s "http://${alias}/global-envs" | jq -r '.PUBLIC_IP')
+
+    # Check if the result is not empty and not "null"
+    if [[ -z "$result" || "$result" == "null" ]]; then
+        # If result is empty or "null", return an error code
+        return 1
+    else
+        # Echo the result so that it can be captured by retry_request
+        echo "$result"
+        return 0
+    fi
+}
 
 case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_PRATER in
 "goerli-geth.dnp.dappnode.eth")
@@ -45,11 +105,18 @@ esac
 
 # 1. Execute the dvf_key_tool to generate the keys if they don't exist
 if [ -f "$NODE_PUBKEY_FILE_PATH" ]; then
-    echo "Keys already exist. Skipping key generation."
+    echo "[INFO] Keys already exist. Skipping key generation."
 else
-    echo "Keys don't exist. Generating keys..."
+    echo "[INFO] Keys don't exist. Generating keys..."
     dvf_key_tool ${OPERATOR_NETWORK}
-    echo "Keys generated and stored in $NODE_PUBKEY_FILE_PATH"
+
+    # If command returns an error, exit
+    if [ $? -ne 0 ]; then
+        echo "[ERROR] Error generating keys. Exiting..."
+        exit 1
+    fi
+
+    echo "[INFO] Keys generated and stored in $NODE_PUBKEY_FILE_PATH"
 fi
 
 # The keys have the following format:
@@ -60,16 +127,26 @@ fi
 
 # 2. POST the public key to the dappmanager
 NODE_PUBKEY=$(jq -r '.name' $NODE_PUBKEY_FILE_PATH)
-curl -X POST "http://my.dappnode/data-send?key=pubkey&data=${NODE_PUBKEY}"
+if ! retry_request post_pubkey $MAX_RETRIES $SLEEP_DURATION; then
+    echo "[ERROR] All attempts to post node public key to dappmanager failed after $MAX_RETRIES retries."
+    exit 1
+fi
 
 # 3. Wait until the operator is registered in https://testnet.safestake.xyz/
 if [ -z "$OPERATOR_ID" ]; then
-    echo "OPERATOR_ID is empty. Register in https://testnet.safestake.xyz/ and set your operator ID in the package configuration and click UPDATE."
+    echo "[INFO] OPERATOR_ID is empty. Register in https://testnet.safestake.xyz/ and set your operator ID in the package configuration and click UPDATE."
     sleep infinity
 else
-    echo "OPERATOR_ID has been set to $OPERATOR_ID."
+    echo "[INFO] OPERATOR_ID has been set to $OPERATOR_ID."
 fi
 
-# 3. Start operator
-NODE_IP=$(curl -s http://my.dappnode/global-envs | jq -r '.PUBLIC_IP')
+# 4. Start operator
+NODE_IP=$(retry_request get_public_ip $MAX_RETRIES $SLEEP_DURATION)
+if [[ -z "$NODE_IP" ]]; then
+    echo "[ERROR] Failed to retrieve PUBLIC_IP from dappmanager after $MAX_RETRIES retries. Exiting..."
+    exit 1
+fi
+
+echo "[INFO] NODE_IP set to $NODE_IP"
+
 dvf validator_client --debug-level=info --network=${OPERATOR_NETWORK} --beacon-nodes=${BEACON_NODE_ENDPOINT} --api=${API_SERVER} --ws-url=${EXECUTION_CLIENT_WS} --ip=${NODE_IP} --id=${OPERATOR_ID} --registry-contract=${REGISTRY_CONTRACT_ADDRESS} --network-contract=${NETWORK_CONTRACT_ADDRESS} --base-port=26000 2>&1
